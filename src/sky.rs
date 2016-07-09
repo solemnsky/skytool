aa49aceb3b586da94d8b708b59c36fd0b2c6800b
xalri <lewis@xa1.uk>
2016-07-09 16:54:50 +0100

    move solemnsky code from libaltx to here

---
+++ src/sky.rs
@@ -0,0 +1,110 @@
+#[macro_use] extern crate log;
+extern crate rustc_serialize;
+extern crate libaltx;
+extern crate zip;
+extern crate math;
+
+use math::Vec2d;
+use std::fs::File;
+use std::path::*;
+use std::io::Result;
+use rustc_serialize::json;
+
+pub struct Environment{
+    pub map: Option<Map>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug,RustcDecodable,RustcEncodable)]
+pub struct Map{
+    pub dimensions:  Vec2d,
+    pub obstacles:   Vec<Obstacle>,
+    pub spawnPoints: Vec<Spawn>,
+}
+
+#[derive(Debug,RustcDecodable,RustcEncodable)]
+pub struct Angle{ pub angle: f32 }
+
+impl Angle{
+    pub fn new(mut angle: f32) -> Angle{
+        angle -= 90.;
+        if angle < 0. { angle += 360.; }
+        Angle{ angle: angle }
+    }
+}
+
+#[derive(Debug,RustcDecodable,RustcEncodable)]
+pub struct Spawn{
+    pub pos:   Vec2d,
+    pub angle: Angle,
+    pub team:  u16,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug,RustcDecodable,RustcEncodable)]
+pub struct Obstacle{
+    pub pos: Vec2d,
+    pub localVertices: Vec<Vec2d>,
+    pub damage: i32,
+}
+
+impl Environment{
+    pub fn new() -> Environment{
+        Environment{ map: None }
+    }
+
+    pub fn to_sky(&self, path: &PathBuf) -> Result<()>{
+        use std::io::Write;
+        use zip::CompressionMethod as Compress;
+        let file = try!(File::create(path));
+        let mut zip = zip::ZipWriter::new(file);
+
+        if let Some(ref map) = self.map {
+            try!(zip.start_file("map.json", Compress::Deflated));
+            try!(write!(zip, "{}", json::encode(map).unwrap()));
+        }
+
+        Ok(())
+    }
+}
+
+impl Map{
+    pub fn from_altx(name: &str) -> Map{
+        Map::from_alt(&libaltx::map::Map::from_altx(name))
+    }
+
+    pub fn from_alt(map: &libaltx::map::Map) -> Map{
+        debug!("writing solemnsky map");
+        for v in &map.views{
+            if v.name == "Game"{
+                let dimensions = v.bounds[1];
+                let mut obstacles = Vec::new();
+                let mut spawn_points = Vec::new();
+                for g in &v.geometry{
+                    if g.collidable{
+                        obstacles.push(Obstacle{
+                            pos: g.pos,
+                            localVertices: g.hull.clone(),
+                            damage: 1,
+                        });
+                    }
+                }
+                let mut main_team: i32 = -1;
+                for i in 0..v.spawn_points.len(){
+                    for s in &v.spawn_points[i]{
+                        if main_team == -1 { main_team = i as i32; }
+                        let team = if i as i32 == main_team { 1 } else { 2 };
+                        spawn_points.push(Spawn{
+                            pos: s.0, angle: Angle::new(s.1.round()), team: team as u16 });
+                    }
+                }
+                return Map{
+                    dimensions:  dimensions,
+                    obstacles:   obstacles,
+                    spawnPoints: spawn_points,
+                };
+            }
+        }
+        unimplemented!();
+    }
+}
--- src/skytool.rs
+++ src/skytool.rs
@@ -2,13 +2,14 @@
 #![plugin(docopt_macros)]
 
 #[macro_use] extern crate log;
-extern crate rustc_serialize;
 extern crate logger;
-extern crate libaltx;
+extern crate sky;
+extern crate rustc_serialize;
 extern crate docopt;
 
 use std::path::Path;
-use libaltx::solemnsky::*;
+use std::fs;
+use sky::*;
 
 docopt!(Args derive Debug, "
 Usage:
@@ -24,20 +25,22 @@ fn main(){
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
     logger::init();
 
-    info!("{:?}", args);
-
     if args.cmd_convert {
+        fs::create_dir_all(&args.flag_output)
+            .expect("Failed to create output directory");
         let path = Path::new(&args.arg_file);
         let file_name = path.file_name().unwrap().to_str().unwrap();
+
         if file_name.ends_with(".altx") {
+
             let map_name = &file_name[0..file_name.len()-5];
             let out = Path::new(&args.flag_output)
                 .join(&format!("{}.sky", map_name));
-            info!("Converting {:?} to {:?}",path,out);
+
+            info!("Converting {} to {:?}", map_name, out);
 
             let mut env = Environment::new();
-            env.map = Some(Map::from_alt(
-                    &libaltx::map::Map::from_altx(map_name)));
+            env.map = Some(Map::from_altx(map_name));
 
             env.to_sky(&out).unwrap();
         } else {
